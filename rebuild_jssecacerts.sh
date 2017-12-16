9fa30e7e276f16cc980737b79df1139c6b45e520
good-paste <good-paste@users.noreply.github.com>
2017-12-16 13:23:20 -0500

    Create rebuild_jssecacerts.sh

---
+++ rebuild_jssecacerts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mv /etc/vco/app-server/security/jssecacerts /etc/vco/app-server/security/jssecacerts.bak
+keytool -keystore /etc/vco/app-server/security/jssecacerts -storepass echo `cat /var/lib/vco/keystore.password` -alias dunes -genkey -keyalg RSA -validity 3650
+chown vco:vco /etc/vco/app-server/security/jssecacerts
+vro-configure.sh reset-authentication
+service vco-configurator restart
